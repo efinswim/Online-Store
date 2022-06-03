70b7499379d08a1e9a82159d046b106316b3a838
Andrii Rastorhuiev <efinswim@gmail.com>
2022-06-04 01:03:58 +0300

    added markup navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
 import { BakeryDining } from '@mui/icons-material';
-import { AppBar, Container, Toolbar, Typography, styled } from '@mui/material';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  styled,
+  TextField,
+  IconButton,
+  Badge,
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 import React from 'react';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
-  justifyContent: 'space-between',
 });
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 15px',
+  margin: '10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '300px',
+}));
 
 function Navbar() {
   return (
@@ -19,18 +37,35 @@ function Navbar() {
           />
           <Typography
             variant='h6'
-            noWrap
             component='a'
             href='/'
             sx={{
               mr: 2,
-              display: { xs: 'none', sm: 'flex' },
+              display: { xs: 'none', sm: 'none', md: 'flex' },
               fontWeight: 700,
               color: 'inherit',
               textDecoration: 'none',
             }}>
             ONLINE STORE JUNK
           </Typography>
+            <Search sx={{ display: { xs: 'none', sm: 'flex' }, mr: 1 }}>
+              <TextField
+                fullWidth
+                onInput={(e) => {
+                  console.log(e.target.value);
+                }}
+                variant='standard'
+                placeholder='Search...'
+              />
+              <IconButton edge='end' type='submit'>
+                <SearchIcon color='primary' />
+              </IconButton>
+            </Search>
+            <IconButton edge='end' type='submit'>
+              <Badge badgeContent={4} color='secondary'>
+                <ShoppingCartIcon sx={{ color: 'white' }} />
+              </Badge>
+            </IconButton>
         </StyledToolbar>
       </Container>
     </AppBar>
